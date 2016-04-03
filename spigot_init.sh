08f0a654c0f4ac09d0b30c8d7c9780faa61a1f10
Character animation tools <zixt233@gmail.com>
2016-04-03 21:06:47 +0800

    Update spigot_init.sh

--- spigot_init.sh
+++ spigot_init.sh
@@ -26,7 +26,7 @@ chown -R minecraft.minecraft /$SPIGOT_HOME/
 
 
 cd /$SPIGOT_HOME/
-su - minecraft -c 'java -Xms512M -Xmx512M -XX:MaxPermSize=128M -jar spigot.jar'
+su - minecraft -c 'java -Xms500M -Xmx500M -XX:MaxPermSize=128M -jar spigot.jar'
 
 bash
 
