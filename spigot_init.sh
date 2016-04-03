b7952036fc23976f98a1ebdf2f935adc0e7038f6
Character animation tools <zixt233@gmail.com>
2016-04-03 20:02:07 +0800

    Update spigot_init.sh

--- spigot_init.sh
+++ spigot_init.sh
@@ -13,11 +13,8 @@ fi
 #only build if jar file does not exist
 if [ ! -f /$SPIGOT_HOME/spigot.jar ]; then 
   echo "Building spigot jar file, be patient"
-  mkdir -p /$SPIGOT_HOME/build
-  cd /$SPIGOT_HOME/build
-  wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-  HOME=/$SPIGOT_HOME/build java -jar BuildTools.jar
-  cp /$SPIGOT_HOME/build/Spigot/Spigot-Server/target/spigot-1.*.jar /$SPIGOT_HOME/spigot.jar
+  cd /$SPIGOT_HOME/
+  mv /spigot.jar /$SPIGOT_HOME/
 
   #accept eola
   echo "eula=true" > /$SPIGOT_HOME/eula.txt
@@ -29,7 +26,7 @@ chown -R minecraft.minecraft /$SPIGOT_HOME/
 
 
 cd /$SPIGOT_HOME/
-su - minecraft -c 'java -Xms512M -Xmx1536M -XX:MaxPermSize=128M -jar spigot.jar'
+su - minecraft -c 'java -Xms512M -Xmx512M -XX:MaxPermSize=128M -jar spigot.jar'
 
 # fallback to root and run shell if spigot don't start/forced exit
 bash
