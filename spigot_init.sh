fa5687c4ea7b088845ab17905649a044fbc9535a
Character animation tools <zixt233@gmail.com>
2016-06-20 14:43:49 +0800

    Update spigot_init.sh

--- spigot_init.sh
+++ spigot_init.sh
@@ -10,21 +10,10 @@ if [ "$EULA" != "true" ]; then
   exit
 fi
 
-#only build if jar file does not exist
-if [ ! -f /$SPIGOT_HOME/spigot.jar ]; then 
-  echo "Building spigot jar file, be patient"
-  cd /$SPIGOT_HOME/
-  mv /spigot.jar /$SPIGOT_HOME/
-
-  #accept eola
-  echo "eula=true" > /$SPIGOT_HOME/eula.txt
-
-fi
-
 chown -R minecraft.minecraft /$SPIGOT_HOME/
 
 
 cd /$SPIGOT_HOME/
-su - minecraft -c "java $MINECRAFT_OPTS -jar spigot.jar"
+su - minecraft -c "java $MINECRAFT_OPTS -jar paperclip.jar"
 bash
 
